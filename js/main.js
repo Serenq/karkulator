260b131ed25cfb07ffe3e0e57aa11cbca94ee9f2
Serenq <tiotiokan@gmail.com>
2023-04-27 23:05:14 +0300

    скрипты переписаны под классовый стиль

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* 
-    КАРКУЛЯТОР 1.2 / 18 апреля 2023 / by Serenq
+    КАРКУЛЯТОР 1.3 / 27 апреля 2023 / by Serenq
     
     Основные возможности:
     - Проверка ввода.
@@ -9,115 +9,121 @@
     - После нажатия кнопки (=), можно продолжать вычисления.
 */
 
-//CALC.EXE
-;(function(){
-    const btn = $('.btn');
-    const input = $('.display-result__input');
-    const noticeVal = $('.display-result__notice');
-    const placeholder = ['Каркулятор','2 + 2 * 2 = 8','Красота','Готов!','Приятный результат'];
-    // Убирает лишние символы. Защита от ввода хуйни всякими ебланами.
-    const reg_allowedType = /\B[^\-\d\.\s]+|[^\d\+\-\*\/\.]|(?<=\d+[\+\-\*\/\.])[\+\*\/\.]+|(?<=\d+\-)\-+|\B[\+\-\*\/][\+\-\*\/]+|(?<=0+)0+\.|00+\.[0-9]+|(?<=[\+\-\*\/])0+[1-9]+|^00+[1-9\.\+\-\*\/]+|(?<=\.[0-9]+)\.|(?<=\.)\.+|(?<=\.)\-|\B\./g;
-    // Проверка уже введённого для вывода подсказки БЕЗ ОШИБОК!
-    const reg_formattedVal = /^\-?[0-9]+$|^(?:[0-9\.]+|\-[0-9])(?:(?:[\+\-\*\/])(?:0?\.?[1-9]+|0))+$|^0?[1-9\.]+$/;
-
-    let calc = {
-        value: '',
-        executed: false,
-        about: 'Karkulator 1.2 by Serenq / 18 apr 2023',
-        placehold: function(){
-            let rand = Math.floor(Math.random() * placeholder.length);
-            input.attr('placeholder', placeholder[rand]);
-        },
-        clickToInput: function(e){
-            calc.value += $(this).attr('data-btn') || '';
-
-            // Очистить инпут по нажатию КЛЕР
-            if( $(this).hasClass('btn-clr') ){ calc.clear() }
-
-            // Если в строке уже есть выражение. Нужно вычислить
-            if( $(this).hasClass('btn-execute') ){
-                calc.result();
-                noticeVal.text('...');
-            }
-            
-            // Ввод данных
-            input.val( calc.value ); // Ввод данных
-            calc.reg_allowInput(); // Форматирование ввода
-            calc.notice(); // Предварительный просмотр результата
-        },
-        typeToInput: function(e){
-            calc.value = input.val(); // Ввод данных
-            calc.reg_allowInput(); // Форматирование ввода
-            calc.notice(); // Предварительный просмотр результата
-
-            if(e.which == 32){ calc.clear() } // SPACE
-
-            // Enter - Результат
-            if(e.which == 13){ calc.result() } // ENTER
-        },
-        reg_allowInput: function(){
-            calc.value = input.val().replace(reg_allowedType, '');
-            input.val(calc.value);
-        },
-        reg_formatStr: function(){
-            calc.value = input.val().replace(reg_allowedType, '');
-            input.val(calc.value);
-        },
-        clear: function(){
-            calc.value = '';
-            calc.placehold();
-            input.val(calc.value);
-            noticeVal.text('...');
-        },
-        result: function(){
-            let reg = new RegExp(/[\+\-\*\/\.]\.?$/,'gm'); // Убрать символы в конце
-            calc.reg_allowInput(); // Форматирование ввода
-            calc.reg_formatStr(); // Форматирование операторов от лишних символов
-            calc.value = calc.value.replace(reg,''); // Убрать символы в конце
-            calc.value = eval(calc.value);
-            input.val( calc.value );
-        },
-        notice: function(){
-            // Предварительный просмотр результата. Если форматированный результат верен.
-            if( reg_formattedVal.test(calc.value) ){
-                noticeVal.text(eval(calc.value));
-            }
-        },
-        versionLog: function(){
-            console.log("%c" + this.about, "color: #ddd; font-style: italic; background-color: #444; padding: 0 20px");
+class Calculator {
+    constructor(){
+        this.value = '';
+        this.el_buttons = $('.btn');
+        this.el_input = $('.display-result__input');
+        this.el_notice = $('.display-result__notice');
+        this.el_skinBtn = $('.skin-switch__btn');
+        this.placeholder = ['Каркулятор','2 + 2 * 2 = 8','Красота','Готов!','Приятный результат'];
+        this.about = {
+            title: 'Karkulator',
+            ver: 1.3,
+            author: 'Serenq',
+            date: '27 apr 2023'
         }
-    };
+        this.timer_dayUpdate = undefined;
 
-    let colorSkins = {
-        element: undefined,
-        timer: undefined,
-        translate: function(input){
+        // Убирает лишние символы. Защита от ввода хуйни всякими ебланами.
+        this.reg_allowedType = /\B[^\-\d\.\s]+|[^\d\+\-\*\/\.]|(?<=\d+[\+\-\*\/\.])[\+\*\/\.]+|(?<=\d+\-)\-+|\B[\+\-\*\/][\+\-\*\/]+|(?<=0+)0+\.|00+\.[0-9]+|(?<=[\+\-\*\/])0+[1-9]+|^00+[1-9\.\+\-\*\/]+|(?<=\.[0-9]+)\.|(?<=\.)\.+|(?<=\.)\-|\B\./g;
+        // Проверка уже введённого для вывода подсказки БЕЗ ОШИБОК!
+        this.reg_formattedVal = /^0$|(?:\-?[0-9]*?\.?[0-9]+)(?:[\+\-\*\/]?(?:[0-9]*?\.?[0-9]+))$/;
+
+        this.el_buttons.on('click', this.clickButtons);
+        this.el_input.on('keyup', this.typeString);
+        this.el_skinBtn.on('click', this.skinChange);
+    }
+
+    timeDependence(){
+        let hours = new Date().getHours();
+
+        function setTheme(){
+            if((hours >= 20 && hours <= 23) || (hours >= 0 && hours <= 7)){$('#calc').attr('class', 'skin-' + 'dark');} // С 20:00 до 7:00 - ночная тема
+        }
+        setTheme();
+
+        this.timer_dayUpdate = setInterval(setTheme, 10000);
+    }
+
+    skinChange = (e) => {
+        let getSkinData = $(e.currentTarget).attr('data-skin');
+
+        function translate(input){
             return input == 'light' ? 'Светлая'
                 : input == 'dark'? 'Тёмная'
                 : input == 'nephrite' ? 'Нефритовая' : '...';
-        },
-        timeDependence: function(){
-            let hours = new Date().getHours();
+        }
 
-            function setTheme(){
-                if((hours >= 20 && hours <= 23) || (hours >= 0 && hours <= 7)){$('#calc').attr('class', 'skin-' + 'dark');} // С 20:00 до 7:00 - ночная тема
-            }
-            setTheme();
+        $('#calc').attr('class', 'skin-' + getSkinData);
+        this.el_notice.text( 'Тема: ' + translate(getSkinData) );
+        clearInterval(this.timer_dayUpdate);
+    }
 
-            this.timer = setInterval(setTheme, 10000);
-            calc.versionLog();
-        },
-        clickOnElem: function(){
-            this.element = $(this).attr('data-skin');
-            $('#calc').attr('class', 'skin-' + this.element);
-            noticeVal.text( 'Тема: ' + colorSkins.translate(this.element) );
-            clearInterval(colorSkins.timer);
+    reg_formatData(){
+        this.value = this.el_input.val().replace(this.reg_allowedType, '');
+        this.el_input.val(this.value);
+    }
+
+    set_placeholder = () => {
+        let rand = Math.floor(Math.random() * this.placeholder.length);
+        this.el_input.attr('placeholder', this.placeholder[rand]);
+    }
+
+    clickButtons = (e) => {
+        this.value += $(e.currentTarget).attr('data-btn') || '';
+
+        // Очистить инпут по нажатию КЛЕР
+        if( $(e.currentTarget).hasClass('btn-clr') ){ this.clear() }
+
+        // Если в строке уже есть выражение. Нужно вычислить
+        if( $(e.currentTarget).hasClass('btn-execute') ){
+            this.result();
+            this.el_notice.text('...');
+        }
+
+        // Ввод данных
+        this.el_input.val( this.value ); // Ввод данных
+        this.reg_formatData(); // Форматирование ввода
+        this.notice(); // Предварительный просмотр результата
+    }
+
+    typeString = (e) => {
+        this.value = this.el_input.val(); // Ввод данных
+        this.reg_formatData(); // Форматирование ввода
+        this.notice(); // Предварительный просмотр результата
+
+        if(e.which == 32){ this.clear() } // Space - Очистить
+        if(e.which == 13){ this.result() } // Enter - Результат
+    }
+
+    result(){
+        let reg = new RegExp(/[\+\-\*\/\.]\.?$/,'gm'); // Убрать символы в конце
+        this.reg_formatData(); // Форматирование ввода
+        this.value = this.value.replace(reg,''); // Убрать символы в конце
+        this.value = eval(this.value);
+        this.el_input.val( this.value );
+    }
+
+    notice(){
+        // Предварительный просмотр результата. Если форматированный результат верен.
+        if( this.reg_formattedVal.test(this.value) ){
+            this.el_notice.text(eval(this.value));
         }
     }
 
-    colorSkins.timeDependence();
+    clear(){
+        this.value = '';
+        this.set_placeholder();
+        this.el_input.val(this.value);
+        this.el_notice.text('...');
+    }
 
-    btn.on('click', calc.clickToInput);
-    input.on('keyup', calc.typeToInput);
-    $('.skin-switch__btn').on('click', colorSkins.clickOnElem);
-}());//CALC.EXE
+    init(){
+        console.log(`%c${this.about.title} ${this.about.ver} by ${this.about.author} / ${this.about.date}`, "color: #ace600; font-style: italic; background-color: #444; padding: 0 20px");
+        this.set_placeholder();
+        this.timeDependence();
+    }
+}
+
+new Calculator().init();
